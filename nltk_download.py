f0c8f6a02b5d425b0a9396831dc8a0d2733fb60f
Chitransh2708 <chitranshy6@gmail.com>
2025-12-13 18:46:15 +0530

    9th commit

--- nltk_download.py
+++
@@ -1,8 +0,0 @@
-import nltk
-import os
-
-NLTK_DIR = "/opt/render/nltk_data"
-os.makedirs(NLTK_DIR, exist_ok=True)
-
-nltk.download("punkt", download_dir=NLTK_DIR)
-nltk.download("stopwords", download_dir=NLTK_DIR)
