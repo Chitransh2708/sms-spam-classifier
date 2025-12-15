95db1ed281f9d471c26540687927fff9c6ecaa4f
Chitransh2708 <chitranshy6@gmail.com>
2025-12-15 11:08:52 +0530

    go to initial commit

--- app.py
+++ app.py
@@ -1,72 +1,50 @@
 import streamlit as st
 import pickle
 import string
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import nltk
 
-# ---------------------------
-# Simple Stemmer (lightweight)
-# ---------------------------
-class SimpleStemmer:
-    def stem(self, word):
-        suffixes = ['ing', 'ly', 'ed', 'ious', 'ies', 'ive', 'es', 's', 'ment']
-        for suf in suffixes:
-            if word.endswith(suf) and len(word) > len(suf)+2:
-                return word[:-len(suf)]
-        return word
-
-ps = SimpleStemmer()
-
-# ---------------------------
-# Stopwords (common English)
-# ---------------------------
-STOPWORDS = {
-    'i','me','my','myself','we','our','ours','ourselves','you','your','yours',
-    'yourself','yourselves','he','him','his','himself','she','her','hers',
-    'herself','it','its','itself','they','them','their','theirs','themselves',
-    'what','which','who','whom','this','that','these','those','am','is','are',
-    'was','were','be','been','being','have','has','had','having','do','does',
-    'did','doing','a','an','the','and','but','if','or','because','as','until',
-    'while','of','at','by','for','with','about','against','between','into',
-    'through','during','before','after','above','below','to','from','up','down',
-    'in','out','on','off','over','under','again','further','then','once','here',
-    'there','when','where','why','how','all','any','both','each','few','more',
-    'most','other','some','such','no','nor','not','only','own','same','so',
-    'than','too','very','s','t','can','will','just','don','should','now'
-}
-
-# ---------------------------
-# Text Preprocessing Function
-# ---------------------------
+ps = PorterStemmer()
 def transform_text(text):
     text = text.lower()
-    words = text.split()
-    cleaned = [w.strip(string.punctuation) for w in words if w.strip(string.punctuation) and w not in STOPWORDS]
-    stemmed = [ps.stem(w) for w in cleaned]
-    return " ".join(stemmed)
+    text = nltk.word_tokenize(text)
+    y=[]
+    for i in text:
+        if i.isalnum():
+            y.append(i)
 
-# ---------------------------
-# Load vectorizer and model
-# ---------------------------
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+        
+    
+    return " ".join(y)
+
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
-# ---------------------------
-# Streamlit UI
-# ---------------------------
-st.title("Email/SMS Spam Classifier")
+st.title("Email/sms Spam Classifier")
 
 input_sms = st.text_area("Enter the message to classify:")
-
 if st.button('Predict'):
-    # 1. Preprocess
-    transform_sms = transform_text(input_sms)
-    
-    # 2. Vectorize
+
+#1, preprocess
+    transform_sms= transform_text(input_sms)
+#2, vectorize
     vector_input = tfidf.transform([transform_sms])
-    
-    # 3. Predict
+#3, predict
     result = model.predict(vector_input)[0]
-    
-    # 4. Display result
+#4, display
     if result == 1:
         st.header("Spam")
     else:
